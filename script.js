6a3d7983ae818a6f6543ea903528252cf5b6e021
samane <samane_zarini@yahoo.com>
2024-04-25 20:28:04 +0330

    add transition

--- script.js
+++ script.js
@@ -17,7 +17,24 @@ const showAbout = function () {
     about.classList.remove('hidden');
 }
 
-                                             
 btnAbout.addEventListener('click', showAbout);
 btnHome.addEventListener('click', showHome);
 
+
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("section1").classList.add("show");
+});
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    document.getElementById("btn-about").addEventListener("click", function () {
+        document.getElementById("section2").classList.add("show");
+        document.getElementById("section1").classList.remove("show");
+    });
+
+    document.getElementById("btn-home").addEventListener("click", function () {
+        document.getElementById("section1").classList.add("show");
+        // حذف کلاس "show" از سکشن دیگر
+        document.getElementById("section2").classList.remove("show");
+    });
+});
